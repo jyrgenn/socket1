6334506ddffadb2bc77bf036c583f4b10c753241
ni <>
2003-01-07 07:35:18 +0000

    updated copyright notice.

--- utils.c
+++ utils.c
@@ -1,9 +1,9 @@
-/* This file is part of Socket-1.2p1.
+/* This file is part of Socket-1.3.
  */
 
 /*-
- * Copyright (c) 1992, 1999, 2000 Juergen Nickelsen <ni@jnickelsen.de>
- * All rights reserved.
+ * Copyright (c) 1992, 1999, 2000, 2001, 2002, 2003
+ * Juergen Nickelsen <ni@jnickelsen.de>. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
